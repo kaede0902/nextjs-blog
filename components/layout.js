10eb64cc5aceea01f516bccb3c613972563950a6
kaede0902 <kaede0920js@gmail.com>
2020-07-29 10:50:44 +0900

    mod layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,18 @@
+import Head from 'next/head'
 import styles from './layout.module.css'
+import Link from 'next/link'
 
-export default function Layout ({ children }) {
-  return <div className={styles.container}>{children}</div>
+const name = 'kaede0902';
+export const siteTitle = 'Next.js Sample Website'
+
+
+export default function Layout ({ children, home }) {
+  return ( 
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico">
+      </Head>
+      {children}
+    </div> 
+  )
 }
