3d2acc9c1060f60b03ba7e2f1913d407d576d16b
kaede0902 <kaede0920js@gmail.com>
2020-09-17 18:48:21 +0900

    update to api

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
 
 export default function Home({ allPostsData }) {
   return (
@@ -35,15 +29,25 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map( ({ id, date, title,  }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href='/posts/[id]' as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                {date}
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
   )
+}
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
